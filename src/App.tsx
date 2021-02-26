4b5a9edbddb05119c9d3875fc991cdbe21927b44
Arqam Ali <arqam.ali16@gmail.com>
2021-02-27 02:14:21 +0400

    menu open

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonRouterOutlet, IonApp } from '@ionic/react';
+import { IonRouterOutlet, IonApp, IonMenu, IonHeader, IonToolbar, IonMenuButton, IonPage } from '@ionic/react';
+import { menuController } from '@ionic/core';
 import '../src/index.css';
 
 import { IonReactRouter } from '@ionic/react-router';
@@ -9,19 +10,30 @@ import Login from './pages/Login';
 import { getToken } from '../src/Api';
 
 const App: React.FC = () => {
+	menuController.enable(true);
 	return (
 		<IonApp>
-			<IonReactRouter>
-				<IonRouterOutlet>
-					<Route path='/login' exact>
-						<Login />
-					</Route>
-					<Route path='/private'>
-						<AppTab />
-					</Route>
-					<Redirect from='/' to='/private/home' exact />
-				</IonRouterOutlet>
-			</IonReactRouter>
+			<IonMenu side='end' contentId='main-content' type='overlay'>
+				<IonHeader>
+					<IonToolbar color='secondary'>
+						<IonMenuButton slot='end' color='primary' />
+					</IonToolbar>
+				</IonHeader>
+				<div>Hi</div>
+			</IonMenu>
+			<IonPage id='main-content'>
+				<IonReactRouter>
+					<IonRouterOutlet>
+						<Route path='/login' exact>
+							<Login />
+						</Route>
+						<Route path='/private'>
+							<AppTab />
+						</Route>
+						<Redirect from='/' to='/private/home' exact />
+					</IonRouterOutlet>
+				</IonReactRouter>
+			</IonPage>
 		</IonApp>
 	);
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,9 +22,11 @@ import {
 	IonMenu,
 	IonButtons,
 	IonMenuButton,
+	IonButton,
 } from '@ionic/react';
 import React from 'react';
 import moment from 'moment';
+import { menuController } from '@ionic/core';
 import { checkmarkCircle, helpCircle, cardOutline } from 'ionicons/icons';
 
 import Medison from '../../src/assets/madison.jpg';
@@ -35,85 +37,70 @@ import _ from 'lodash';
 
 const Home: any = () => {
 	const { payments, userDetails } = useValues(PaymentsLogic);
-	console.log(userDetails);
 	return (
 		<>
-			<IonMenu side='start' menuId='first'>
-				<IonHeader>
-					<IonToolbar color='primary'>
-						<IonTitle>Start Menu</IonTitle>
-					</IonToolbar>
-				</IonHeader>
-				<IonContent>
-					<IonList>
-						<IonItem>Menu Item</IonItem>
-						<IonItem>Menu Item</IonItem>
-						<IonItem>Menu Item</IonItem>
-						<IonItem>Menu Item</IonItem>
-						<IonItem>Menu Item</IonItem>
-					</IonList>
-				</IonContent>
-			</IonMenu>
-			<IonPage>
-				<IonHeader className='header'>
-					<IonToolbar className='toolbar'>
-						<IonButtons slot='start'>
-							<IonMenuButton color='dark'></IonMenuButton>
-						</IonButtons>
-						<IonTitle>Home</IonTitle>
-					</IonToolbar>
-				</IonHeader>
-				<IonContent fullscreen>
-					<IonCard>
-						<IonCardHeader>
-							<IonAvatar slot='start'>
-								<img src='https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' />
-							</IonAvatar>
-							<IonCardSubtitle>Welcome</IonCardSubtitle>
-							<IonCardTitle>{userDetails.name}</IonCardTitle>
-							<IonCardSubtitle>{userDetails.email}</IonCardSubtitle>
-						</IonCardHeader>
-					</IonCard>
-					<IonList>
-						<IonListHeader>Recent Payments</IonListHeader>
-						{_.map(payments, (eachPayment) => (
-							<IonCard>
-								<IonItem>
-									<IonIcon
-										icon={cardOutline}
-										style={{
-											color: eachPayment.approved ? '#bae637' : '#ffa39e',
-											fontSize: '40px',
-										}}
-									></IonIcon>
+			<IonHeader className='header'>
+				<IonToolbar className='toolbar'>
+					<IonButtons slot='start'>
+						<IonMenuButton
+							color='dark'
+							autoHide={false}
+							onClick={() => menuController.open()}
+						></IonMenuButton>
+					</IonButtons>
+					<IonTitle>Home</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+				<IonCard>
+					<IonCardHeader>
+						<IonAvatar slot='start'>
+							<img src='https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' />
+						</IonAvatar>
+						<IonCardSubtitle>Welcome</IonCardSubtitle>
+						<IonCardTitle>{userDetails.name}</IonCardTitle>
+						<IonCardSubtitle>{userDetails.email}</IonCardSubtitle>
+					</IonCardHeader>
+				</IonCard>
+				<IonList>
+					<IonListHeader>Recent Payments</IonListHeader>
+					{_.map(payments, (eachPayment) => (
+						<IonCard>
+							<IonItem>
+								<IonIcon
+									icon={cardOutline}
+									style={{
+										color: eachPayment.approved ? '#bae637' : '#ffa39e',
+										fontSize: '40px',
+									}}
+								></IonIcon>
 
-									<IonLabel>
-										<IonGrid>
-											<IonRow>
-												<IonCol size='7'>
-													<h2 style={{ fontWeight: 400 }}>
-														{moment(eachPayment.paid_on).format('DD MMMM YYYY')}
-													</h2>
-													<h3>{eachPayment.amount}</h3>
-													<p>{eachPayment.mode}</p>
-												</IonCol>
-												<IonCol>
-													<IonChip
-														color={eachPayment.approved ? 'success' : 'danger'}
-														style={{ float: 'left' }}
-													>
-														<IonLabel>{eachPayment.approved ? 'PAID' : 'PENDING'}</IonLabel>
-													</IonChip>
-												</IonCol>
-											</IonRow>
-										</IonGrid>
-									</IonLabel>
-								</IonItem>
-							</IonCard>
-						))}
-					</IonList>
-				</IonContent>
-			</IonPage>
+								<IonLabel>
+									<IonGrid>
+										<IonRow>
+											<IonCol size='7'>
+												<h2 style={{ fontWeight: 400 }}>
+													{moment(eachPayment.paid_on).format('DD MMMM YYYY')}
+												</h2>
+												<h3>{eachPayment.amount}</h3>
+												<p>{eachPayment.mode}</p>
+											</IonCol>
+											<IonCol>
+												<IonChip
+													color={eachPayment.approved ? 'success' : 'danger'}
+													style={{ float: 'left' }}
+												>
+													<IonLabel>{eachPayment.approved ? 'PAID' : 'PENDING'}</IonLabel>
+												</IonChip>
+											</IonCol>
+										</IonRow>
+									</IonGrid>
+								</IonLabel>
+							</IonItem>
+						</IonCard>
+					))}
+				</IonList>
+			</IonContent>
 		</>
 	);
 };
